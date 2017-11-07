93f31fbe518727865aebd3dc68083a52797373b2
Jack Gonser <jackgonser@gmail.com>
2017-11-06 16:34:58 -1000

    Added team 8898 code to master

---
+++ Team8898/src/main/java/org/firstinspires/ftc/team8898/OpenCVAuto.java
@@ -0,0 +1,82 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.team8898;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+
+import ftc.vision.BeaconColorResult;
+import ftc.vision.FrameGrabber;
+import ftc.vision.ImageProcessorResult;
+
+@Autonomous(name = "TestVision" , group = "Vision")
+@Disabled
+public class OpenCVAuto extends LinearOpMode {
+
+
+    static final double COUNTS_PER_MOTOR_REV = 1440.0;
+    static final double WHEEL_DIAMETER_INCHES = 4.0;
+    static final double COUNTS_PER_INCH = COUNTS_PER_MOTOR_REV /(WHEEL_DIAMETER_INCHES * Math.PI);
+
+    private ElapsedTime runtime = new ElapsedTime();
+    DcMotor vertical = null;
+    DcMotor grab = null;
+
+    FrameGrabber frameGrabber = FtcRobotControllerActivity.frameGrabber;
+
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        FrameGrabber frameGrabber = FtcRobotControllerActivity.frameGrabber; //Get the
+
+        frameGrabber.grabSingleFrame(); //Tell it to grab a frame
+        while (!frameGrabber.isResultReady()) { //Wait for the result
+            Thread.sleep(5); //sleep for 5 milliseconds
+        }
+        //Get the result
+        ImageProcessorResult imageProcessorResult = frameGrabber.getResult();
+        BeaconColorResult result = (BeaconColorResult) imageProcessorResult.getResult();
+        BeaconColorResult.BeaconColor leftColor = result.getLeftColor();
+        BeaconColorResult.BeaconColor rightColor = result.getRightColor();
+        telemetry.addData("Result", leftColor); //Display it on telemetry
+        telemetry.update();
+//wait before quitting (quitting clears telemetry)
+        Thread.sleep(1000);
+
+    }
+
+}
---
+++ Team8898/src/main/java/org/firstinspires/ftc/team8898/WaffleToasterAutoBlue.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.team8898;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+
+import ftc.vision.BeaconColorResult;
+import ftc.vision.FrameGrabber;
+import ftc.vision.ImageProcessorResult;
+
+/**
+ * Created by Jack Gonser on 10/16/2017.
+ */
+@Autonomous (name = "WaffleToasterAutoBlue", group = "WaffleAuto")
+@Disabled
+public class WaffleToasterAutoBlue extends WaffleToasterAuto
+{   // sets up the basic wheel variables
+    private DcMotor leftFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+    // creates arm variable
+    private DcMotor arm = null;
+    // sets up variable for the arm servos
+    private Servo leftServo = null;
+    private Servo rightServo = null;
+    // sets up variable for the jewel servo
+    private Servo jewelServo = null;
+
+    private BeaconColorResult.BeaconColor leftColorCheck = null;
+    private BeaconColorResult.BeaconColor rightColorCheck = null;
+    //sets basic variables to set servos and motors to
+    private float stop = 0;
+    private float forward = 1;
+    private float backward = -1;
+    private float halfBack = -0.5f;
+    private float halfForward = 0.5f;
+
+    private int halfSec = 500;
+    private int sec = 1000;
+
+    public void runOpMode ()  throws InterruptedException{ //set up for the phone tp
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+        leftServo = hardwareMap.get(Servo.class, "leftServo");
+        rightServo = hardwareMap.get(Servo.class, "rightServo");
+
+        jewelServo = hardwareMap.get(Servo.class, "jewelServo");
+
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftServo.setDirection(Servo.Direction.FORWARD);
+        rightServo.setDirection(Servo.Direction.REVERSE);
+
+        jewelServo.setDirection(Servo.Direction.FORWARD);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            waitForStart();
+
+            FrameGrabber frameGrabber = FtcRobotControllerActivity.frameGrabber; //Get the
+
+            frameGrabber.grabSingleFrame(); //Tell it to grab a frame
+            while (!frameGrabber.isResultReady()) { //Wait for the result
+                Thread.sleep(5); //sleep for 5 milliseconds
+            }
+            //Get the result
+            ImageProcessorResult imageProcessorResult = frameGrabber.getResult();
+            BeaconColorResult result = (BeaconColorResult) imageProcessorResult.getResult();
+            BeaconColorResult.BeaconColor leftColor = result.getLeftColor();
+            BeaconColorResult.BeaconColor rightColor = result.getRightColor();
+            telemetry.addData("Result", leftColor); //Display it on telemetry
+            telemetry.update();
+
+            rightColorCheck = rightColor;
+            leftColorCheck = leftColor;
+
+            if (leftColorCheck == BeaconColorResult.BeaconColor.BLUE) { //senses if the red jewel is on the left
+                telemetry.addData("Going for the jewel on the left.","Blue Jewel is on the right");
+                telemetry.update();
+                leftServo.setPosition(0.25);
+                rightServo.setPosition(0.25);
+                jewelServo.setPosition(forward);
+                arm.setPower(0.25);
+                Thread.sleep(secondsToMillis(0.5));
+                arm.setPower(0);
+                allDrive(halfBack);
+                Thread.sleep(secondsToMillis(0));
+                jewelServo.setPosition(backward);
+                Thread.sleep(secondsToMillis(0.5));
+                allDrive(halfForward);
+                Thread.sleep(secondsToMillis(2.5));
+                allDrive(stop);
+            } else if (rightColorCheck == BeaconColorResult.BeaconColor.BLUE) { //senses if the blue jewel is on the left
+                telemetry.addData("Going for the jewel on the left.","Blue Jewel is on the left");//adds data to package to send towards the driver station
+                telemetry.update();//sends package to the driver station
+                leftServo.setPosition(0.25);
+                rightServo.setPosition(0.25);
+                jewelServo.setPosition(forward);
+                arm.setPower(0.25);
+                Thread.sleep(secondsToMillis(0.5));
+                arm.setPower(0);
+                allDrive(halfForward);
+                Thread.sleep(secondsToMillis(2));
+                jewelServo.setPosition(stop);
+                allDrive(stop);
+            }
+        }
+    }
+}
